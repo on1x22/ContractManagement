2137271883da8d5fefb8d95bcd4ae3f68437c5f3
on1x22 <on1x22@mail.ru>
2023-11-05 18:14:56 +0400

    Frontend refactoring

--- CMFrontend/CMFrontend/src/app/myimport/myimport.component.ts
+++ CMFrontend/CMFrontend/src/app/myimport/myimport.component.ts
@@ -5,18 +5,18 @@ import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http
   selector: 'myimport-app',
   templateUrl: './myimport.component.html',
   styleUrls: ['./myimport.component.css']
-  //template: `<h3>Страница импорта</h3>`
 })
 export class MyimportComponent {
   fileName!: string;
   isCorrectFileType!: boolean;
-  isCorrectUploaded!: boolean;
+  isCorrectUploaded!: boolean | null;
   formData!: FormData;
   file!: File;
+  uploadMessage!: string;
 
   constructor(private http: HttpClient) { }
 
-  onFileSelected($event: any) {
+  async onFileSelected($event: any) {
 
     this.file = $event.target.files[0];
 
@@ -35,45 +35,17 @@ export class MyimportComponent {
       this.formData = new FormData();
 
       this.formData.append("fileExcel", this.file, this.fileName);
-
-      //const upload$ = this.http.post("/api/thumbnail-upload", formData);
-
-      //upload$.subscribe();
-      let asda = 123;
     }
   }
 
   uploadFile() {
-    const httpOptions = {
-      headers: new HttpHeaders({
-        //'Content-Type': 'multipart/form-data',
-        //'Accept': 'application/json'
-      })
-    };
-
-    // Взято здесь https://blog.angular-university.io/angular-file-upload/
-    //const upload = this.http.post("https://localhost:7203/api/Import/UploadFile", this.formData, httpOptions);
-    /*const upload =*/ return this.http.post("/Import/UploadFile", this.formData/*, httpOptions*/).subscribe(result => {
-      // do something, if upload success
-      let str = result;
-      console.log("ok " + result);
+    return this.http.post("/Import/UploadFile", this.formData).subscribe(result => {
+      this.uploadMessage = "Файл успешно загружен на сервер";
       this.isCorrectUploaded = true;
     }, error => {
-      if (error instanceof HttpErrorResponse && (error as HttpErrorResponse).status == 200)
-      {
-        this.isCorrectUploaded = true;
-      }
-      if (error instanceof HttpErrorResponse && (error as HttpErrorResponse).status == 500) {
-        this.isCorrectUploaded = false;
-      }
-      console.log("ok " + error);
-    }
-    );
-
-
-
-    //upload.subscribe();
-
-    let asda = 123;
+      this.uploadMessage = (error as HttpErrorResponse).error;
+      this.isCorrectUploaded = false;      
+      console.log(error);
+    });
   }
 }
